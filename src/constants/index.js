b4ad69e3cef988aaee1e30a9037a4e12a1888184
vblanc <vicbr92@gmail.com>
2022-05-19 18:25:45 +0200

    add constants.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const POKEAPI_URL = 'https://pokeapi.co/api/v2/pokemon/';
-export const LIMIT = 10;
-export let OFFSET = 0;
+export const DEFAULT_LIMIT = 10;
+export const DEFAULT_OFFSET = 0;
 export const INIT_TEXT =
   '¡Oh! Parece que aún no hay Pokémon que mostrar. Prueba con ese botón de ahí abajo, ¡abre la pokédex!';
 export const BUTTON_COLOR = '#08979c';
